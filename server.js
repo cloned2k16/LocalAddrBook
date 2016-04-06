96131d64671fc61db6fdbba8db0d630cb862b2ca
i5 <know@ledg.es>
2016-04-06 16:13:50 +0200

    cleaning up

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 // server.js
 
     // ====================================================== Set Up
-    var express         = require('express');
-    var app             = express();     
-    var router          = express.Router();     
-    var morgan          = require('morgan');                                // log requests to the console (express4)
-    
-                                                                   
-    var DRY             = DRY       || {};                                  // collecting DRY Stuff here 
-        DRY.APP         = DRY.APP   || {};
-    
-        DRY.log         = function log              (s)      {
-            console.log.apply(this,arguments);
-        }
-    // ====================================================== Handy short-cuts 
-    var _               = _         || {};
-        _.LOG           = DRY.log;
+    var express             = require('express');
+    var app                 = express();     
+    var router              = express.Router();     
+    var morgan              = require('morgan');                            // log requests to the console (express4)
     // ====================================================== Configuration
-    var PUBLIC_HTML     = '/public_html'; 
-        DRY.listenPort  = 1111;
+    var _APP                = {};
+        _APP.PUBLIC_HTML    = '/public_html'; 
+        _APP.LISTEN_PORT    = 1111;
+        _APP.log            = function log() { console.log.apply(this,arguments); }
+        
+    var _                   = _APP;
     // ====================================================== Application
-    app.use(express.static(__dirname + PUBLIC_HTML));                       //
+    app.use(express.static(__dirname + _.PUBLIC_HTML));                     //
     app.use(morgan('dev'));                                                 // log every request to the console
     app.use(function(req, res){ res.sendStatus(404);});                     // simply NOT FOUND
     // ====================================================== Main Loop
-    app.listen(DRY.listenPort);
-    DRY.log("Express server listening on http://localhost:"+DRY.listenPort+"/");
+    app.listen(_.LISTEN_PORT);
+    _.log("Express server listening on http://localhost:"+_.LISTEN_PORT+"/");
  
