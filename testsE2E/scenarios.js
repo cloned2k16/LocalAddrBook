68353f792adbb0302a79a966358ee1833566866b
i5 <know@ledg.es>
2016-04-08 17:03:50 +0200

    cleaning up ...

--- testsE2E/scenarios.js
+++ testsE2E/scenarios.js
@@ -2,50 +2,44 @@
 
 describe('LocalAddrBook', function() {
 
-        it ('should stay on the home page', function() {
-            browser.get('/');
-            expect(browser.getLocationAbsUrl()).toMatch("");
-        });
+    it ('should stay on the home page', function() {
+      browser.get('/');
+      expect(browser.getLocationAbsUrl()).toMatch("");
+    });
 
 
-    describe('FirstName error display'                                , function() {
-
+    describe('check for validation messages to be in place'             , function() {
         beforeEach(function() {
-            browser.get('/');
         });
 
-
-        it('should check errors in First Name', function() {
-            expect(element.all(by.css('[ng-view] div')).first().getText()).
-            toMatch('First Name is required');
+        var errMessages = element.all(by.css('div [ng-message]'));
+      
+        it('should show error in First Name', function() {
+            expect(errMessages.get(0).getText()).toMatch('First Name is required');
         });
 
+        it('should show error in Last Name', function() {
+            expect(errMessages.get(1).getText()).toMatch('Last Name is required');
+        });
+
+        it('should show error in eMail', function() {
+            expect(errMessages.get(2).getText()).toMatch('EMail is required');
+        });
+
+        it('should show error in country selection', function() {
+            expect(errMessages.get(3).getText()).toMatch('Please select a location');
+        });
+        
     });
   
-    describe('LastName error display'                                 , function() {
-
-        beforeEach(function() {
-        browser.get('/');
-        });
-
-
-        it('should check errors in Last Name', function() {
-            var el=element(by.id('lastNameErr'));
-            expect(el.getText()).
-            toMatch('Last Name is required');
-        });
-    
-
-    });
   
     describe('AddAddress Form validation'                             , function() {
         beforeEach(function() {
-         //browser.pause(); 
         });
   
         browser.get('/');
         var people      = element.all   (by.repeater    ('addr in people'));
-        var form        = element       (by.id          ('formAddAddress'));
+        var form        = element       (by.css         ('.newAddr'));
         var firstName   = element       (by.model       ('data.firstName' ));
         var lastName    = element       (by.model       ('data.lastName' ));
         var eMail       = element       (by.model       ('data.eMail'     ));
@@ -107,7 +101,6 @@ describe('LocalAddrBook', function() {
             expect(element.all  (bttnByCSS).count()).toEqual(1);
             element             (bttnByCSS).click();
             expect(people.count()).toEqual(0); 
-            //browser.pause();
         });
         
         // .... and more ...
